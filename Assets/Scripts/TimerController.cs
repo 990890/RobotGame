e9fd3cd515fe94a92ebc65fdf7b060253f600e16
neevir <neevirtournaments@gmail.com>
2018-04-18 20:17:13 +0200

    Added TimeManager [Level1]
    - The game will now stop when the timer reaches 0

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour {
+
+
+    #region Fields
+
+    [SerializeField]
+    private float timer = 120;
+
+    [SerializeField]
+    private Text timerText;
+    #endregion
+
+    #region Methods
+
+    private void Update()
+    {
+        UpdateTimer();
+    }
+
+    private void UpdateTimer() {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            timer = 0;
+            Time.timeScale = 0;
+        }
+        var seconds = timer % 60;
+        var minutes = Mathf.FloorToInt(timer / 60);
+        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+
+    }
+    #endregion
+
+}
